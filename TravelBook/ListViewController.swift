291de6d95031223d45a99c36e90e8a145f257942
Onur Anıl <onuranil749@gmail.com>
2024-05-25 16:27:59 +0300

    commit added

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Onur Anıl on 16.04.2024.
+//
+
+import UIKit
+import CoreData
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var titleArray = [String]()
+    var idArray = [UUID]()
+    
+    var chosenTitle = ""
+    var chosenTitleId : UUID?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Sag ust koseye + BUTTONU eklemek icin
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+     
+        getData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newPlace"), object: nil)
+    }
+    
+    
+    // Veri cekmek icin
+   @objc func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do {  // Sonuclar
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let title = result.value(forKey: "title"){
+                        self.titleArray.append(title as! String)
+                    }
+                    
+                    if let id = result.value(forKey: "id"){
+                        self.idArray.append(id as! UUID)
+                    }
+                    
+                    tableView.reloadData()
+                    
+                }
+                
+            }
+            
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+    @objc func addButtonClicked(){
+        chosenTitle = ""
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var context = cell.defaultContentConfiguration()
+        context.text = titleArray[indexPath.row]
+        cell.contentConfiguration = context
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenTitle = titleArray[indexPath.row]
+        chosenTitleId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toViewController"{
+            let destinationVC = segue.destination as! ViewController
+            destinationVC.selectedTitle = chosenTitle
+            destinationVC.selectedTitleID = chosenTitleId
+        }
+    }
+    
+}
